94fbc0e56752f0232c28f1e8741c56ba7ff5d99f
luckstraw <aviles.jamesluck.ecc.bscs@gmail.com>
2025-04-04 12:45:41 +0800

    backend files added

---
+++ backend/server/signup.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+
+include "db.php";
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode(["status" => "success"]);
+} else {
+    echo json_encode(["status" => "error", "message" => $conn->error]);
+}
+
+$conn->close();
+?>
